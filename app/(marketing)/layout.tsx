ccaa1cb8beb8db08ef07a563db207e2ec711e45c
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-06-24 12:20:06 +0500

    bug solve

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,7 +3,11 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 
-export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Navbar />
